42bacc51937deeb90f25bcaa887f93b3dfdbd5d5
harmolipi <nikob381@gmail.com>
2022-04-20 14:08:01 -0400

    Created solver functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -7,13 +7,94 @@ class SudokuSolver {
         return true;
     }
 
-    checkRowPlacement(puzzleString, row, column, value) {}
+    checkRowPlacement(puzzleString, row, column, value) {
+        const rowString = puzzleString.substring(row * 9, row * 9 + 9);
+        const rowValues = rowString.split('');
+        if (rowValues.includes(value)) {
+            return false;
+        }
+        return true;
+    }
 
-    checkColPlacement(puzzleString, row, column, value) {}
+    checkColPlacement(puzzleString, row, column, value) {
+        const colValues = puzzleString.split('').filter((_, i) => i % 9 === column);
+        if (colValues.includes(value)) {
+            return false;
+        }
+        return true;
+    }
 
-    checkRegionPlacement(puzzleString, row, column, value) {}
+    checkRegionPlacement(puzzleString, row, column, value) {
+        const regionRow = Math.floor(row / 3) * 3;
+        const regionCol = Math.floor(column / 3) * 3;
+        const regionValues = puzzleString.split('').filter((_, i) => {
+            const rowIndex = Math.floor(i / 9);
+            const colIndex = i % 9;
+            return (
+                rowIndex >= regionRow &&
+                rowIndex < regionRow + 3 &&
+                colIndex >= regionCol &&
+                colIndex < regionCol + 3
+            );
+        });
+        if (regionValues.includes(value)) {
+            return false;
+        }
+        return true;
+    }
 
-    solve(puzzleString) {}
+    solve(puzzleString) {
+        if (!this.validate(puzzleString)) {
+            return false;
+        }
+        const puzzleArray = puzzleString.split('');
+        for (let i = 0; i < puzzleArray.length; i++) {
+            const row = Math.floor(i / 9);
+            const column = i % 9;
+            const value = puzzleArray[i];
+            if (value === '.') {
+                for (let j = 1; j <= 9; j++) {
+                    if (
+                        this.checkRowPlacement(puzzleString, row, column, j) &&
+                        this.checkColPlacement(puzzleString, row, column, j) &&
+                        this.checkRegionPlacement(puzzleString, row, column, j)
+                    ) {
+                        puzzleArray[i] = j;
+                        const newPuzzleString = puzzleArray.join('');
+                        const result = this.solve(newPuzzleString);
+                        if (result) {
+                            return result;
+                        }
+                        puzzleArray[i] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return puzzleArray.join('');
+    }
+
+    check(puzzleString) {
+        if (!this.validate(puzzleString)) {
+            return false;
+        }
+        const puzzleArray = puzzleString.split('');
+        for (let i = 0; i < puzzleArray.length; i++) {
+            const row = Math.floor(i / 9);
+            const column = i % 9;
+            const value = puzzleArray[i];
+            if (value === '.') {
+                return false;
+            }
+            if (!this.checkRowPlacement(puzzleString, row, column, value) ||
+                !this.checkColPlacement(puzzleString, row, column, value) ||
+                !this.checkRegionPlacement(puzzleString, row, column, value)
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 module.exports = SudokuSolver;
