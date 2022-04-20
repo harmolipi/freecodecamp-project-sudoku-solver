d7b29874f001d46cf25385fa4b3e8ee8bef8bfe9
harmolipi <nikob381@gmail.com>
2022-04-20 14:08:11 -0400

    Added /check and /solve routes

--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,23 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+module.exports = function(app) {
+    let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
-
+    app.route('/api/check').post((req, res) => {
+        const puzzleString = req.body.puzzle;
+        if (!puzzleString) return res.json({ error: 'Required field missing' });
+        const solution = solver.check(puzzleString);
+        return res.json({
+            solution: solution,
+        });
     });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
 
+    app.route('/api/solve').post((req, res) => {
+        const puzzleString = req.body.puzzle;
+        const result = solver.solve(puzzleString);
+        res.json({
+            result: result,
+        });
     });
 };
